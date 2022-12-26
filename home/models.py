7a520368c6488f40cc28e15d0fbd564125b24d3a
morteza <mortezashoeibi77@gmail.com>
2022-12-26 20:10:02 +0330

    create the Sweat model

--- home/models.py
+++ home/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Sweat(models.Model):
+    title = models.CharField(max_length=5000)
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'...{self.title} | {self.text[:15]}'
