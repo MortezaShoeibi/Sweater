af2fcbf4818514cd1a51cb6858de7cc3c4ca6570
morteza <mortezashoeibi77@gmail.com>
2022-12-26 21:57:20 +0330

    set pagination

--- home/views.py
+++ home/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from .models import Sweat
+from django.core.paginator import Paginator
 
 
 def home(request) -> None:
-    sweats = Sweat.objects.order_by("-created_at")
+    objects_list = Sweat.objects.order_by("-created_at")
+    paginator = Paginator(objects_list, 4)
+    page_num = request.GET.get('page')
+    sweats = paginator.get_page(page_num)
     return render(request, 'home/home.html', {'sweats': sweats})
