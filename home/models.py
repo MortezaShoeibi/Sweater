a2fa90924439e490f130606b80e3e556663b3b2e
morteza <mortezashoeibi77@gmail.com>
2022-12-27 16:04:15 +0330

    add about page

--- home/models.py
+++ home/models.py
@@ -7,5 +7,18 @@ class Sweat(models.Model):
     text = tiny_models.HTMLField()
     created_at = models.DateTimeField(auto_now_add=True)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f'...{self.title}  |  {self.text[:15]}'
+
+
+class About(models.Model):
+    text = tiny_models.HTMLField()
+    """
+     the latest object of this model will be showed in the template,
+     so every time creating a new one will be the last update.
+    """
+    last_update = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return f'...{self.last_update}  |  {self.text[:15]}'
+    
--- home/views.py
+++ home/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Sweat
+from .models import Sweat, About
 from django.core.paginator import Paginator
 
 
@@ -9,3 +9,10 @@ def home(request) -> None:
     page_num = request.GET.get('page')
     sweats = paginator.get_page(page_num)
     return render(request, 'home/home.html', {'sweats': sweats})
+
+
+def about(request) -> None:
+    about = About.objects.last()
+    pre_about = About.objects.filter(id=about.id - 1)
+    pre_about.delete()
+    return render(request, 'home/about.html', {'about': about})
