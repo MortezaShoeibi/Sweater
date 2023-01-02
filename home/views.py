1388d703dcaf24852923d4979246f87eb06656cc
morteza <mortezashoeibi77@gmail.com>
2023-01-02 14:59:28 +0330

    change views functions type extentions to HttpResponse

--- home/views.py
+++ home/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from .models import Sweat, About
 from django.core.paginator import Paginator
+from django.http.response import HttpResponse
 
 
-def home(request) -> None:
+def home(request) -> HttpResponse:
     objects_list = Sweat.objects.order_by("-created_at")
     paginator = Paginator(objects_list, 3)
     page_num = request.GET.get('page')
@@ -11,7 +12,7 @@ def home(request) -> None:
     return render(request, 'home/home.html', {'sweats': sweats})
 
 
-def about(request) -> None:
+def about(request) -> HttpResponse:
     about = About.objects.last()
     if about != None:
         pre_about = About.objects.filter(id=about.id - 1)
